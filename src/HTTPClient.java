84bcd964fcbfbe9bef65fbf22b55c9024374e52f
arnedb <arne.debrabandere@student.kuleuven.be>
2015-02-27 20:24:35 +0100

    Properdere versie van HTTPClient

--- src/HTTPClient.java
+++ src/HTTPClient.java
@@ -14,64 +14,103 @@ class HTTPClient {
   public static void main(String[] args) throws Exception {
     if (args.length != 4)
       printHelp();
-    URI uri = getURI(args[1]);
-    parseCommand(args);
+
+    // get arguments
+    String command = args[0];
+    String uriString = args[1];
+    String portString = args[2];
+    String version = args[3];
+
+    // get URI object from uriString
+    URI uri = getURI(uriString);
+
+    // get port int
+    int port = Integer.parseInt(portString);
+
+    parseCommand(command,uri,port,version);
   }
 
   /**
-   * Print the description of how to specify the arguments.
+   * Print the description of how to specify the program arguments.
    */
   public static void printHelp() {
 
       System.out.println("Description of arguments..."); // TODO
   }
 
+  /**
+   * Get URI object from given URI string
+   * @param uriString String value of the given URI
+   */
   private static URI getURI(String uriString) throws Exception {
     // TODO nakijken of er http:// voostaat of niet
     return new URI(uriString);
   }
 
-  private static void parseCommand(String[] args) throws Exception {
-
-    switch (args[0]) {
-       case "HEAD" : head(args[1], args[2], args[3]);
-           break;
-       /*case "GET" : get(args[1], args[2], args[3]);
-           break;
-       case "PUT" : put(args[1], args[2], args[3]);
-           break;
-       case "POST" : post(args[1], args[2], args[3]);
-           break;*/
+  /**
+   * Parse the command.
+   * @param command command string
+   * @param uri URI object
+   * @param port port number
+   * @param version http version (1.0 or 1.1)
+   */
+  private static void parseCommand(String command, URI uri, int port, String version) {
+    try {
+      switch (command) {
+        case "HEAD":
+          head(uri, port, version);
+          break;
+         /*case "GET" : get(uri, port, version);
+                        break;
+         case "PUT" : put(uri, port, version);
+                      break;
+         case "POST" : post(uri, port, version);
+                       break;*/
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
     }
   }
 
-  private static void head(String uri, String stringPort, String httpversion) throws Exception {
-      int port = Integer.parseInt(stringPort);
-      URI uriObject = new URI(uri);
-      String path = uriObject.getPath();
-      String host = uriObject.getHost();
-      Socket clientSocket = new Socket(host, port);
+  /**
+   * Command: HEAD (uri) (port) (version)
+   * @param uri URI object
+   * @param port port number
+   * @param version http version (1.0 or 1.1)
+   * @throws Exception
+   */
+  private static void head(URI uri, int port, String version) throws Exception {
+    String path = uri.getPath(); // path to file
+    String host = uri.getHost();
 
-      // Create outputstream (convenient data writer) to this host.
-      DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+    // Connect to the host.
+    Socket clientSocket = null;
+    try {
+      clientSocket = new Socket(host, port);
+    } catch (IOException e) {
+      System.out.println("Unable to connect to " + host + ":" + port);
+    }
 
-      // Create an inputstream (convenient data reader) to this host
-      BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    // Create outputstream (convenient data writer) to this host.
+    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-      outToServer.writeBytes("HEAD " + path + " HTTP/" + httpversion + "\r\n\r\n");
+    // Create an inputstream (convenient data reader) to this host
+    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-      // Read text from the server and write it to the screen.
-      String response = "";
-      while ((response = inFromServer.readLine()) != null) {
+    // Send HTTP command to server.
+    outToServer.writeBytes("HEAD " + path + " HTTP/" + version + "\r\n\r\n");
 
-          System.out.println(response);
+    // Read text from the server and write it to the screen.
+    String response = "";
+    while ((response = inFromServer.readLine()) != null) {
 
-      }
+        System.out.println(response);
 
+    }
 
-      // Close the socket.
-      clientSocket.close();
+    // Close the socket.
+    clientSocket.close();
   }
 
-
-  } // End of class HTTPClient
+}
