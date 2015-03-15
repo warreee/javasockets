54dd3e82257cf992588883334c1568566cca9d46
Ward Schodts <wardschodts@hotmail.com>
2015-03-15 23:47:53 +0100

    HTTPServerTest added

---
+++ src/HTTPServerTest.java
@@ -0,0 +1,15 @@
+/*
+ * A simple example TCP Server application
+ *
+ * Computer Networks, KU Leuven.
+ *
+ * Arne De Brabandere
+ * Ward Schodts
+ */
+public class HTTPServerTest {
+
+    public static void main(String argv[]) throws Exception {
+
+    }
+
+}
