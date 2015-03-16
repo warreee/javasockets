7051d3aeb590ff9ff61cd5d0903aeffa96c7bdc1
Ward Schodts <wardschodts@hotmail.com>
2015-03-16 18:19:24 +0100

    continue reading

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -3,23 +3,75 @@
  */
 public class CommandParser {
 
+
+
+    public CommandParser(){
+
+    }
+
+
+
     public Command parseCommand(String command){
 
+
+        MainCommand mainCommand;
+
+        switch (getMainCommand(command)) {
+            case HEAD:
+                break;
+            case POST:
+                break;
+            case GET:
+                System.out.println();
+            case PUT:
+                break;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns false if its a GET or HEAD request, is used to see if we need to continue read the input stream.
+     * @param command
+     * @return
+     */
+    public boolean continueReading(String command){
+        if (getMainCommand(command).equals(MainCommand.GET) || getMainCommand(command).equals(MainCommand.HEAD)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private enum MainCommand {
+        HEAD,
+        POST,
+        GET,
+        PUT
+    }
+
+    /**
+     * Geeft het hoofdcommando terug
+     * @param command in stringvorm
+     * @return
+     */
+    private MainCommand getMainCommand(String command){
         String mainCommand = command.substring(command.indexOf(" "));
 
         switch (mainCommand){
             case "HEAD":
                 command = command.substring(4, command.length() - 1); //Removes mainCommand
-                break;
+                System.out.println(command);
+                return MainCommand.HEAD;
             case "GET":
                 command = command.substring(3, command.length() - 1); //Removes mainCommand
-                break;
+                return MainCommand.GET;
             case "POST":
                 command = command.substring(4, command.length() - 1); //Removes mainCommand
-                break;
+                return MainCommand.POST;
             case "PUT":
                 command = command.substring(3, command.length() - 1); //Removes mainCommand
-                break;
+                return MainCommand.PUT;
         }
         return null;
     }
